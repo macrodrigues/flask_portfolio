daec67dd6cc9e6fa43a9192a9abc4a45eac1a864
Marco Rodrigues <marcoacavaco.rodrigues@hotmail.com>
2022-06-02 14:43:59 -0300

    updates with login form

--- test.py
+++ test.py
@@ -1,12 +1,38 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
 import random
+import smtplib
+import datetime as dt
+load_dotenv(os.path.join(os.path.dirname(__file__), 'keys.env'))
 
+
+OWN_EMAIL = os.getenv('mail')
+OWN_PASSWORD = os.getenv('pass')
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    current_year = dt.datetime.now().year
+    return render_template('index.html', year = current_year)
+
+@app.route("/submit", methods=["POST"])
+def receive_data():
+    print(request.method)
+    if request.method == "POST":
+        name = request.form["name"]
+        mail = request.form["mail"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        send_email(name, mail, phone, message)
+        return render_template('submission.html')
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:Contact Portfolio\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP(host='smtp-mail.outlook.com', port=587) as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 if __name__=='__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
