74234c549e0be18f6c1b626d4a4658e8eb98cc99
Marco Rodrigues <marcoacavaco.rodrigues@hotmail.com>
2022-06-07 16:13:42 +0100

    change in port

--- test.py
+++ test.py
@@ -36,6 +36,6 @@ def send_email(name, email, phone, message):
 
 if __name__=='__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    port = 5000 + random.randint(0, 999)
-    url = f"http://127.0.0.1:{port}"
-    app.run(debug=True, port=port)
+    # port = 5000 + random.randint(0, 999)
+    # url = f"http://127.0.0.1:{port}"
+    app.run(debug=True, port=5000, host="0.0.0.0")
