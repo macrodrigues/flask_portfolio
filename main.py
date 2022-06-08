28bf05ebdd533ea5f52eb03a938d15a8930bb397
Marco Rodrigues <marcoacavaco.rodrigues@hotmail.com>
2022-06-08 16:49:47 +0100

    overall updates

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import smtplib
 import datetime as dt
 load_dotenv(os.path.join(os.path.dirname(__file__), 'keys.env'))
 
-
 OWN_EMAIL = os.getenv('mail')
 OWN_PASSWORD = os.getenv('pass')
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,7 +18,6 @@ def hello_world():
 
 @app.route("/submit", methods=["POST"])
 def receive_data():
-    print(request.method)
     if request.method == "POST":
         name = request.form["name"]
         mail = request.form["mail"]
@@ -29,10 +28,11 @@ def receive_data():
 
 def send_email(name, email, phone, message):
     email_message = f"Subject:Contact Portfolio\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    print(email_message)
     with smtplib.SMTP(host='smtp-mail.outlook.com', port=587) as connection:
         connection.starttls()
         connection.login(OWN_EMAIL, OWN_PASSWORD)
-        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message.encode('utf-8'))
 
 if __name__=='__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
